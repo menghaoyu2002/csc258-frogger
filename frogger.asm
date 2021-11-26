c89d5fae4fd5e35d9824a14a559eb9e3c1393b7c
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-26 04:42:13 -0500

    changed screen dimensions

--- frogger.asm
+++ frogger.asm
@@ -5,8 +5,8 @@
 ## Bitmap Display Configuration:
 # - Unit width in pixels: 8
 # - Unit height in pixels: 8
-# - Display width in pixels: 256
-# - Display height in pixels: 256
+# - Display width in pixels: 512
+# - Display height in pixels: 512
 # - Base Address for Display: 0x10008000 ($gp)
 ## Which milestone is reached in this submission?
 # (See the assignment handout for descriptions of the milestones)
