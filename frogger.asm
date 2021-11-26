31daa31f704c9492e00622875f36dd2044f0e687
Menghao Yu <menghaoyu2002@gmail.com>
2021-11-26 04:35:57 -0500

    added preamble

--- frogger.asm
+++ frogger.asm
@@ -1,10 +1,24 @@
-# Demo for painting## Bitmap Display Configuration:
+######################################################################
+# CSC258H5S Fall 2021 Assembly Final Project
+# University of Toronto, St. George
+## Student: Name, Student Number
+## Bitmap Display Configuration:
 # - Unit width in pixels: 8
 # - Unit height in pixels: 8
 # - Display width in pixels: 256
 # - Display height in pixels: 256
 # - Base Address for Display: 0x10008000 ($gp)
-#
+## Which milestone is reached in this submission?
+# (See the assignment handout for descriptions of the milestones)
+# - Milestone 1/2/3/4/5 (choose the one the applies)
+## Which approved additional features have been implemented?
+# (See the assignment handout for the list of additional features)
+# 1. (fill in the feature, if any)
+# 2. (fill in the feature, if any)# 3. (fill in the feature, if any)
+# ... (add more if necessary)## Any additional information that the TA needs to know:
+# - (write here, if any)
+#######################################################################
+
 .data
 	displayAddress:	.word 0x10008000
 .text
@@ -15,6 +29,7 @@
 	sw $t1, 0($t0) # paint the first (top-left) unit red.
 	sw $t2, 4($t0) # paint the second unit on the first row green. Why$t0+4?
 	sw $t3, 128($t0) # paint the first unit on the second row blue. Why +128?
+	
 Exit:
 	li $v0, 10 # terminate the program gracefully
 	syscall
